cb30187b080e905b9e211383b97d81e6b0be9458
Youjingchu <Youjingchu@users.noreply.github.com>
2016-12-19 05:52:16 +0800

    Update server.R

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ shinyServer(function(input, output) {
     T.map@data["index"] <- county_seq[,ncol(county_seq)]
     state_popup <- paste0("<strong>", county_seq$C_Name, "</strong>", "<br><strong>Dead_Ratio: </strong>", T.map$index)
     qpal <- colorQuantile("Blues", T.map$index,n =10)
-    m <- leaflet(T.map) %>%  addProviderTiles("Stamen.TonerLite") %>% setView(lng=120.602, lat=23.680, zoom = 8)
+    m <- leaflet(T.map) %>%  addProviderTiles("Stamen.TonerLite") %>% setView(lng=120.602, lat=23.680, zoom = 7)
     m <- m %>% addPolygons(stroke = FALSE, fillOpacity = 0.5, smoothFactor = 0.5, color = ~qpal(index), weight = 3, popup = state_popup)
     m %>% addLegend("bottomright", pal = qpal, values = ~index, title = "acdnt_dead_ratio", opacity = 0.5)
   })
