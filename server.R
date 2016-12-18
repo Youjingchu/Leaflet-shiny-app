bbe82a175ffc3876dd10252b9bc4f51ae3eaf6a7
Youjingchu <Youjingchu@users.noreply.github.com>
2016-12-19 05:44:48 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(leaflet)
+library(rgdal)
+library(readr)
+library(dplyr)
+library(geojsonio)
+
+T.map <- readOGR(dsn="OFiles", layer="County_MOI_1041215.shp")
+acdnt <- read_csv("OFiles/English_data_for_graphing.csv")
+
+shinyServer(function(input, output) { 
+  output$distPlot1 <- renderLeaflet({
+    county_seq <- T.map@data
+    data <- acdnt %>% select(1,3)
+    county_seq$C_Name <- iconv(county_seq$C_Name,"UTF-8")
+    county_seq <- county_seq %>% merge(data, by.x = c("C_Name"), by.y = c("C_Name")) %>% arrange(OBJECTID)
+    T.map@data["index"] <- county_seq[,ncol(county_seq)]
+    state_popup <- paste0("<strong>", county_seq$C_Name, "</strong>", "<br><strong>Dead_Ratio: </strong>", T.map$index)
+    qpal <- colorQuantile("Blues", T.map$index,n =10)
+    m <- leaflet(T.map) %>%  addProviderTiles("Stamen.TonerLite") %>% setView(lng=120.602, lat=23.680, zoom = 8)
+    m <- m %>% addPolygons(stroke = FALSE, fillOpacity = 0.5, smoothFactor = 0.5, color = ~qpal(index), weight = 3, popup = state_popup)
+    m %>% addLegend("bottomright", pal = qpal, values = ~index, title = "acdnt_dead_ratio", opacity = 0.5)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,7 @@
+shinyUI(fluidPage(
+  titlePanel("User distributed map"),
+  fluidRow(
+    
+  ),
+  mainPanel(leafletOutput("distPlot1", width = "150%", height = "540px"))
+))
