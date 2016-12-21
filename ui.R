efd0ba96a21956cf334ec82a063d0ae16d3a9bb8
Youjingchu <Youjingchu@users.noreply.github.com>
2016-12-21 13:10:23 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,31 +5,24 @@ library(plotly)
 header<-dashboardHeader(title="Situation of Credit Card in Taiwan",titleWidth = 500)
 
 body<-dashboardBody(
-  fluidRow(
+   fluidRow(
     column(width = 6,
-           box(width = NULL, solidHeader = TRUE,
-               leafletOutput("TaiwanMap", height=370)
-           ),
-           box(width=NULL,
-               plotlyOutput("barchart")
-           )
-    ),
-    column(width=6,
            box(width=NULL, 
                uiOutput("yearSelect")
            ),
            box(width=NULL,
-               dataTableOutput("dataTable")
+               plotlyOutput("barchart", height=315)
+           )
+    ),
+    column(width=6,
+           box(width = NULL, solidHeader = TRUE,
+               leafletOutput("TaiwanMap", height=435)
            )
     )
   ),
-  fluidRow(
-    column(width=12,
            box(width=NULL,
                dataTableOutput("dataTable")
            )
-    )
-  )
 )
 
 dashboardPage(
